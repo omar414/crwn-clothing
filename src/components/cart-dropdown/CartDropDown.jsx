c1db360a39d54d5dceb1aa3145e0493dd9248bc6
Omar Sakka amini <omarsakkaamini@gmail.com>
2024-01-04 13:17:33 +0400

    added in our addToCart feature

--- src/components/cart-dropdown/CartDropDown.jsx
+++ src/components/cart-dropdown/CartDropDown.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './CartDropDown.scss'
 import Button from '../button/button'
-const CartDropdown = () => (
+import CartItem from '../cart-item/CartItem';
+import { CartContext } from '../../contexts/cart';
+const CartDropdown = () => {
+  const {cartItems} = useContext(CartContext)
+  return(
     <div className='cart-dropdown-container'>
-      <div className='cart-items' />
+      <div className='cart-items' >
+       { cartItems.map((item )=>( <CartItem key={item.id} CartItem={item}/>))}
+      </div>
       <Button>checkout</Button>
     </div>
-  );
+)};
   
   export default CartDropdown;
